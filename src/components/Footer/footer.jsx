d77a948a88764f98edb8d16b2cb3638505a569e1
niranjan03 <niranjanmaurya03@gmail.com>
2024-09-14 17:19:00 +0530

    new pagess

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Work = () => {
+const footer = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const Work = () => {
   )
 }
 
-export default Work
+export default footer
