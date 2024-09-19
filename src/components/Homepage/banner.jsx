f0f50ed12e41095e66009dc6b0a17d5545826b47
niranjan03 <niranjanmaurya03@gmail.com>
2024-09-19 20:53:37 +0530

    "Refactored CSS styles and updated HTML structure for banner and work components"

--- src/components/Homepage/banner.jsx
+++ src/components/Homepage/banner.jsx
@@ -5,17 +5,19 @@ const banner = () => {
   return (
     <div className='banner-section section-bg '>
         <div className="container">
-        <div className="image-container">
-          <img src="https://via.placeholder.com/150" alt="Ibrahim Memon" />
-        </div>
-        <div className="text-container">
-          <p className="intro">Hello! I am Niranjan Maurya</p>
-          {/* <p className="title">A Designer who</p>
-          <h1 className="heading">Judges a book</h1>
-          <p className="subheading">by its <span className="highlight">cover</span>...</p>
-          <p className="subtitle">Because if the cover does not impress you what else can?</p> */}
-          <h2 className="main-heading">I'm a Front-End Developer.</h2>
-          {/* <p className="body">A self-taught UI/UX designer, functioning in the industry for 3+ years now. I make meaningful and delightful digital products that create an equilibrium between user needs and business goals. </p> */}
+        <div className="row ">
+            <div className="col-md-6 col-lg-6 col-sm-12 ">
+                <div className="banner-content">
+                    <h2 className="text-white">Hello, I am Niranjan Maurya</h2>
+                    <span>I am Front-End Developer</span>
+
+                </div>
+            </div>
+            <div className="col-md-6 col-lg-6 col-sm-12 ">
+                <div className="banner-img">
+                    <img src="" alt="" />
+                </div>
+            </div>
         </div>
       </div>
     </div>
--- src/components/Homepage/work.jsx
+++ src/components/Homepage/work.jsx
@@ -21,7 +21,8 @@ const work = () => {
                       <div class="iframe-container">
                           <iframe src="https://newjyotidecor.com"
                               title="New Jyoti Decor"
-                              width="100%" height="400px">
+                              width="100%" height="300px"
+                               >  
                           </iframe>
                       </div>
                   </div>
