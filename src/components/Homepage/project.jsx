e32a7731a3a7443bba4a48acd3f0f273745f5fdd
niranjan03 <32191814+niranjan03@users.noreply.github.com>
2024-10-24 17:47:58 +0530

    "Removed framer-motion dependency, updated CSS and JSX code for project cards and homepage"

--- src/components/Homepage/project.jsx
+++ src/components/Homepage/project.jsx
@@ -1,16 +1,13 @@
-import React, { useRef } from 'react';
-
-
+import React from 'react';
 import ProjectCard from '../Project/ProjectCard';
-import { motion, useInView } from "framer-motion";
-
+import web from '../../assests/image/img/web1.png'
 
 const Projectdata = [
     {
         id: 1,
     title: "New Jyoti Decor",
     description: "Project 1 description",
-    image: `../../assests/image/img/web1.png`,
+    imageUrl: {web},
     gitUrl: "/",
     previewUrl: "/",
     },
@@ -18,7 +15,7 @@ const Projectdata = [
         id: 2,
     title: "Portfolio Website",
     description: "Project 1 description",
-    image: "../../assests/image/img/web2.png",
+    imageUrl: "../../assests/image/img/web2.png",
     gitUrl: "/",
     previewUrl: "/",
     },
@@ -26,7 +23,7 @@ const Projectdata = [
         id: 3,
     title: "Portfolio Website",
     description: "Project 1 description",
-    image: "",
+    imageUrl: "",
     gitUrl: "/",
     previewUrl: "/",
     }
@@ -34,35 +31,24 @@ const Projectdata = [
 
 
 const Project = () => {
-    const ref = useRef(null);
-    const isInView = useInView(ref, {once:true});
-    const cardVariants = {
-        initial: { y: 50, opacity: 0 },
-        animate: { y: 0, opacity: 1 },
-      };
+    
     
     return (
         <div className='container'>
             <h3>Projects</h3>
-            <div className='row  '>
-            <ul ref={ref} className="col-4 col-md-4">
+            <div className=''>
+            <ul  className="row  d-flex">
                 {Projectdata.map((project, index) => (
-                    <motion.li
-                        key={index}
-                        variants={cardVariants}
-                        initial="initial"
-                        animate={isInView ? "animate" : "initial"}
-                        transition={{ duration: 0.3, delay: index * 0.4 }}
-                    >
+                    <li key={index} className='col-4 col-md-4 '>
                         <ProjectCard
                             key={project.id}
                             title={project.title}
                             description={project.description}
-                            imgUrl={project.image}
+                            img={project.imageUrl}
                             gitUrl={project.gitUrl}
                             previewUrl={project.previewUrl}
                         />
-                    </motion.li>
+                    </li>
                 ))}
             </ul>
             </div>
--- src/components/Project/ProjectCard.jsx
+++ src/components/Project/ProjectCard.jsx
@@ -4,25 +4,50 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub} from '@fortawesome/free-brands-svg-icons'
 import { faEye } from '@fortawesome/free-solid-svg-icons'
 
-const ProjectCard = ({imgurl, title, description, giturl, previewurl}) => {
+const ProjectCard = ({imageUrl, title, description, gitUrl, previewUrl}) => {
   return (
-    <div>
-        <div className='' style={{background:`url(${imgurl})`, backgroundSize:'cover'}}>
-            <div className=''>
-                <Link to={giturl} className=''>
+    <div className="card position-relative" style={{ width: '18rem' }}>
+            {imageUrl && (
+                <img src={imageUrl} className="card-img-top" alt={title} />
+            )}
+            <div className="card-body">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{description}</p>
+                
+            </div>
+            <div className="logos">
+            <Link to={gitUrl} className='logo'>
                 <FontAwesomeIcon icon={faGithub} />
                 </Link>
-                <Link to={previewurl} className=''>
+                <Link to={previewUrl} className='logo'>
                 <FontAwesomeIcon icon={faEye} />
                 </Link>
             </div>
         </div>
-        <div>
-            <h4>{title}</h4>
-            <p>{description}</p>
-        </div>
-    </div>
+
+    
+   
   )
 }
 
 export default ProjectCard;
+
+{/* <div>
+<div className='project-card ' style={{ height: '280px' }}>
+      {imageUrl && (
+        <img src={imageUrl} className="card-img-top" alt={title} />
+    )}
+    <div className=''>
+        <Link to={gitUrl} className=''>
+        <FontAwesomeIcon icon={faGithub} />
+        </Link>
+        <Link to={previewUrl} className=''>
+        <FontAwesomeIcon icon={faEye} />
+        </Link>
+    </div>
+</div>
+<div>
+    <h4>{title}</h4>
+    <p>{description}</p>
+</div>
+</div> */}
